ff30559b702c4c9361d1fb792489877f764fb6d1
ssumika <gsuzuki221@gmail.com>
2023-04-14 00:50:39 +0900

    材料と分量の抽出

--- test.py
+++ test.py
@@ -1,17 +1,33 @@
-import sys
-import MeCab
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
+import MeCab
 
-r =  urllib.request.urlopen('https://cookpad.com/recipe/2221374')
-html = r.read()
-#print(html.decode())
-soup = BeautifulSoup(html,'html.parser')
-t = soup.get_text()
-#print(t) 
+m = MeCab.Tagger('-Owakati')
 
-m = MeCab.Tagger("-Ochasen")
-a = m.parse(t)
-words = [i.split()[0] for i in a.splitlines()]
-print(words) 
-print("MACでも動いたのでヨシ！")
+url = "https://cookpad.com/recipe/2221374"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+recipe_title = soup.find('h1', class_='recipe-title').text.strip()
+print(recipe_title)
+
+ingredients_list = []
+for ingredient in soup.find_all('div', class_='ingredient_row'):
+    ingredient_quantity = ingredient.find('div', class_='ingredient_quantity')
+    ingredient_name = ingredient.find('div', class_='ingredient_name')
+    if ingredient_quantity is not None:
+        ingredient_quantity = ingredient_quantity.text.strip()
+        parsed_ingredient_quantity = m.parse(ingredient_quantity).rstrip('\n')
+    else:
+        parsed_ingredient_quantity = None
+        
+    if ingredient_name is not None:
+        ingredient_name = ingredient_name.text.strip()
+        parsed_ingredient_name = m.parse(ingredient_name).rstrip('\n')
+    else:
+        parsed_ingredient_name = None
+
+    ingredients_list.append({'name': parsed_ingredient_name, 'quantity': parsed_ingredient_quantity})
+
+print(ingredients_list)
